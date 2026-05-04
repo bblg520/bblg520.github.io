307f22fbe74b8614f1e35cde5287af4233abf015
bblg520 <yishundu49@gmail.com>
2026-05-04 18:27:52 -0500

    Delete Random Number Generator directory

--- Random Number Generator/diceRoll.js
+++
@@ -1,8 +0,0 @@
-function rollDie() {
- const randomNumber = Math.floor(Math.random() * (6));
- document.getElementById("result").textContent = randomNumber + 1;
-}
-
-rollDie();
-
-document.getElementById("roll-btn").addEventListener("click", rollDie);
